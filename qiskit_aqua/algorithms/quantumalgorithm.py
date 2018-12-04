3721c6ccd3fddb18875d3aff6389b5f8f660629a
seyon <seyon.sivarajah@cambridgequantum.com>
2018-12-04 19:17:18 +0000

    remove compiler flag

--- qiskit_aqua/algorithms/quantumalgorithm.py
+++ qiskit_aqua/algorithms/quantumalgorithm.py
@@ -215,7 +215,6 @@ class QuantumAlgorithm(Pluggable):
                                 'seed': self._random_seed,
                                 'qobj_id': None,
                                 'hpc': hpc_params,
-                                'compiler': compiler,
                                 'seed_mapper': self._random_seed}
 
         info = "Algorithm: '{}' setup with backend '{}', with following setting:\n {}\n{}".format(
